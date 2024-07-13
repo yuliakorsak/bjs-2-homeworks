c1cfcb738f3820660deed404c7d68cd2f2999d89
Yulia Ermolaeva <168757304+yuliakorsak@users.noreply.github.com>
2024-07-14 02:46:38 +0300

    Загружено ДЗ "Основные понятия"

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,21 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = Math.pow(b, 2) - 4 * a * c;
+  if (d === 0) {
+    arr.push(-b / (2 * a));
+  }
+  else if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthlyPercent = percent / 1200;
+  let mortgageBase = amount - contribution;
+  let monthlyPayment = mortgageBase * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent) ** countMonths) - 1)));
+  let total = (monthlyPayment * countMonths).toFixed(2);
+  return +total;
 }
