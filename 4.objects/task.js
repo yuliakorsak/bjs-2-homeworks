104e18bcd2192578ce8b9dee9403bdcf17baf480
Yulia Ermolaeva <168757304+yuliakorsak@users.noreply.github.com>
2024-07-21 12:13:31 +0300

    Загружено ДЗ "Объекты"

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,29 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.marks) {
+        marks.forEach((mark) => this.marks.push(mark));
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (!this.marks || this.marks.length === 0) {
+        return 0;
+    }
+    return average = this.marks.reduce((sum, mark) => sum + mark, 0) / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
