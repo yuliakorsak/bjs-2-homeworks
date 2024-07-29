b9b111fd93d8568f6a9c58bc5ede56b366973dd3
Yulia Ermolaeva <168757304+yuliakorsak@users.noreply.github.com>
2024-07-29 19:25:13 +0300

    Загружено ДЗ "Обработка исключений и замыкания"

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,56 @@
-﻿
+﻿/* Задача 1 */
+function parseCount(number) {
+    let result = Number.parseFloat(number);
+    if (!result) {
+        throw new Error("Невалидное значение");
+    }
+    return result;
+}
+
+function validateCount(number) {
+    let result;
+    try {
+        result = parseCount(number);
+    } catch (error) {
+        return error;
+    }
+    return result;
+}
+
+/* Задача 2 */
+class Triangle {
+    constructor(side1, side2, side3) {
+        if (side1 + side2 < side3 || side1 + side3 < side2 || side2 + side3 < side1) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        } else {
+            this.side1 = side1;
+            this.side2 = side2;
+            this.side3 = side3;
+        }
+    }
+
+    get perimeter() {
+        return this.side1 + this.side2 + this.side3;
+    }
+
+    get area() {
+        let p = this.perimeter / 2;
+        let area = Math.sqrt(p * (p - this.side1) * (p - this.side2) * (p - this.side3));
+        return +area.toFixed(3);
+    }
+}
+
+function getTriangle(side1, side2, side3) {
+    try {
+        return new Triangle(side1, side2, side3);
+    } catch (error) {
+        return {
+            get perimeter() {
+                return "Ошибка! Треугольник не существует";
+            },
+            get area() {
+                return "Ошибка! Треугольник не существует";
+            }
+        }
+    }
+}
